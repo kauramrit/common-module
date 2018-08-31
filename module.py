1741ff58d2d43e10f96c8de5f60a8efd4870e7d0
kauramrit <amrit6689@gmail.com>
2018-08-31 19:23:23 +0530

    gh

---
+++ module.py
@@ -0,0 +1,12 @@
+#Q.1 print the current day using datetime module.
+
+import datetime
+now=datetime.datetime.now()
+print("current day: %d" %now.day)
+
+#Q.2 open your browser and play a video using webbrowser module in python.
+import webbrowser
+webbrowser.open('http://www.youtube.com/watch?v=9xVp8m0fJSg')
+
+#Q.3 write a program to rename all the files in a directory and convert them into .jpg file format.
+
